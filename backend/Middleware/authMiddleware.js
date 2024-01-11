70b89f4cf1035548c37309a83de6c8532a7625b2
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2024-01-11 19:55:46 +0530

    fix:build

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../Models/userModel.js");
+const User = require("../Models/userModel");
 const asyncHandler = require("express-async-handler");
 
 const protect = asyncHandler(async (req, res, next) => {
